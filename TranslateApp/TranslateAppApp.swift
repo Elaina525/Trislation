f07e773480391b8a31c797415c4cbe24bad84775
Elaina525 <s3880590@student.rmit.edu.au>
2023-08-02 16:08:51 +1000

    Initial Commit

---
+++ TranslateApp/TranslateAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  TranslateAppApp.swift
+//  TranslateApp
+//
+//  Created by Xiaolong Guo on 2/8/2023.
+//
+
+import SwiftUI
+
+@main
+struct TranslateAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
