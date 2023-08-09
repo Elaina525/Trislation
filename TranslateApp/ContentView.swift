fda54660ff2756cf562a99bdea881d4ef669900f
zbg2047 <zbg2047@gmail.com>
2023-08-09 15:42:28 +1000

    Sample of translate history

--- TranslateApp/ContentView.swift
+++ TranslateApp/ContentView.swift
@@ -21,6 +21,34 @@ struct ContentView: View {
             }
             VStack {
                 // Texting & talking Buttons
+                VStack(alignment: .leading) {
+                    RoundedRectangle(cornerRadius: 20)
+                        .stroke(style: StrokeStyle(lineWidth: 2))
+                        .frame(width: 300, height: 100)
+                        .foregroundColor(.black)
+                        .overlay(
+                            VStack(spacing: 10) {
+                                Text("Top Example Text")
+                                    .font(.system(size: 16))
+                                    .foregroundColor(.black)
+                                    .multilineTextAlignment(.leading)
+                                
+                                
+                                Divider()
+                                    .background(Color.gray)
+                                    .frame(height: 1)
+                                
+                                Text("Bottom Example Text")
+                                    .font(.system(size: 16))
+                                    .foregroundColor(.black)
+                            }
+                                .padding()
+                        )
+                    
+                    
+                }
+                .padding()
+                
                 
                 HStack {
                     // Language Switching
