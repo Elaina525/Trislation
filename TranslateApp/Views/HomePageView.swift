35367085451b194e4fbdb562be0002d1953d8861
Elaina525 <s3880590@student.rmit.edu.au>
2023-10-12 07:49:09 +1100

    fix UI and add speech to text function

--- TranslateApp/Views/HomePageView.swift
+++ TranslateApp/Views/HomePageView.swift
@@ -6,116 +6,220 @@ enum PageState {
 }
 
 struct HomePageView: View {
+    @StateObject private var speechToText = SpeechToText()
+    
     @State var originalText: String = ""
     @State var leftLanguage: String = "Auto"
     @State var rightLanguage: String = "Chinese"
-    @State var currentPage: PageState = .home
-
-    private var languages = ["English", "Spanish", "French", "German", "Chinese", "Japanese", "Russian", "Arabic"]
-    private var shortLanguages = ["en", "es", "fr", "de", "zh", "ja", "ru", "ar"]
+    @State var isHidden = true
+    @State var from: String
+    @State var to: String
+    
+    // translatedText is an arry of 3 strings
+    @State var translatedText1: String = ""
+    @State var translatedText2: String = ""
+    @State var translatedText3: String = ""
+    
+    @State private var selectedTab: Int = 0
+    @State var isFavourite = false
+    
+    var translateSources = ["Baidu", "DeepL", "Azure"]
+    var languages = ["English", "Spanish", "French", "German", "Chinese", "Japanese", "Russian", "Arabic"]
+    var shortLanguages = ["en", "es", "fr", "de", "zh", "ja", "ru", "ar"]
     var leftLanguageOptions: [String] { ["Auto"] + languages.filter { $0 != rightLanguage } }
     var rightLanguageOptions: [String] { languages.filter { $0 != leftLanguage } }
-
-    var body: some View {
-        switch currentPage {
-        case .home:
-            VStack {
-                VStack {
-                    TextField("Type here", text: $originalText)
-                        .padding()
-                        .foregroundColor(.black)
-                        .cornerRadius(8)
-                        .onSubmit {
-                            currentPage = .translating(originalText)
-                        }
-                    Spacer()
-                    Button {} label: {
-                        Image(systemName: "mic.fill")
-                            .resizable()
-                            .frame(width: 35, height: 50)
-                    }
-                    .frame(width: 100, height: 100)
-                    .foregroundColor(.white)
-                    .background(.blue)
-                    .cornerRadius(50)
-
-                    HStack {
-                        // Language Switching
-                        Picker("Left Language", selection: $leftLanguage) {
-                            ForEach(leftLanguageOptions, id: \.self) { language in
-                                Text(language)
-                            }
-                        }
-                        .frame(width: 120, height: 35)
-                        .background(Color(UIColor.systemGray4))
-                        .cornerRadius(8)
-
-                        Button(action: {
-                            withAnimation {
-                                swap(&leftLanguage, &rightLanguage)
-                            }
-                        }) {
-                            Image(systemName: "arrow.left.arrow.right")
-                        }
-
-                        Picker("Right Language", selection: $rightLanguage) {
-                            ForEach(rightLanguageOptions, id: \.self) { language in
-                                Text(language)
-                            }
-                        }
-                        .frame(width: 120, height: 35)
-                        .background(Color(UIColor.systemGray4))
-                        .cornerRadius(8)
-                    }
-                    .onChange(of: leftLanguage) { _ in
-                        if leftLanguage == rightLanguage {
-                            rightLanguage = rightLanguageOptions.first!
-                        }
-                    }
-                    .onChange(of: rightLanguage) { _ in
-                        if leftLanguage == rightLanguage {
-                            leftLanguage = leftLanguageOptions.first!
-                        }
-                    }
-                    .padding()
-                }
-                // add rounded corner on top
-                .padding()
-                .background(Color(UIColor.systemGray6))
-                .cornerRadius(20)
+    
+    func fetchTranslation(using translationFunction: @escaping (String, @escaping (String?, Error?) -> Void) -> Void, completion: @escaping (String?, Error?) -> Void) {
+        translationFunction(originalText) { translatedText, error in
+            if let error = error {
+                completion(nil, error)
+                return
             }
-            .edgesIgnoringSafeArea(.bottom)
-
-        case let .translating(text):
-            if text != "" {
-                // from language to short language
-                // for "Auto" -> "auto"
-                let from = leftLanguage == "Auto" ? "auto" : shortLanguages[languages.firstIndex(of: leftLanguage)!]
-                let to = shortLanguages[languages.firstIndex(of: rightLanguage)!]
-                VStack {
-                    HStack {
-                        Image(systemName: "chevron.left")
-                            .font(.system(size: 20))
-                            .foregroundColor(.blue)
-                            .onTapGesture {
-                                currentPage = .home
-                            }
-                            .padding(.leading, 16) // Adjust leading padding to move the image to the left
-                    
-                        Spacer() // Add spacer to push the image to the left
-                    }
-                    
-                    TranslateResultView(originalText: text, from: from, to: to)
-                     
-                }
-                
+            guard let translatedText = translatedText else {
+                completion(nil, NSError(domain: "TranslationError", code: -1, userInfo: nil))
+                return
+            }
+            completion(translatedText, nil)
+        }
+    }
+    
+    func fetchTranslations() {
+        
+        
+        fetchTranslation(using: { text, completion in
+            baiduTranslate(text: text, from: from, to: to) { translatedText, error in
+                completion(translatedText, error)
+            }
+        }) { baiduTranslatedText, _ in
+            if let baiduTranslatedText = baiduTranslatedText {
+                translatedText1 = baiduTranslatedText
+                print("Baidu: \(translatedText1)")
             }
         }
+        
+        
+        fetchTranslation(using: { text, completion in
+            deeplTranslate(text: text, from: from, to: to) { translatedText, error in
+                completion(translatedText, error)
+            }
+        }) { deeplTranslatedText, _ in
+            if let deeplTranslatedText = deeplTranslatedText {
+                translatedText2 = deeplTranslatedText
+                print("DeepL: \(translatedText2)")
+            }
+        }
+        
+        fetchTranslation(using: { text, completion in
+            azureTranslate(text: text, from: from, to: to) { translatedText, error in
+                completion(translatedText, error)
+            }
+        }) { azureTranslatedText, _ in
+            if let azureTranslatedText = azureTranslatedText {
+                translatedText3 = azureTranslatedText
+                print("Azure: \(translatedText3)")
+            }
+        }
+        
+        
+        
+    }
+    
+    var body: some View {
+        //        switch currentPage {
+        //        case .home:
+        VStack {
+            VStack {
+                
+                TextField("Type here", text: $originalText)
+                    .padding()
+                    .foregroundColor(.black)
+                    .cornerRadius(8)
+                    .onSubmit {
+                        fetchTranslations()
+                        if isHidden == true {
+                            isHidden.toggle()
+                        }
+                        
+                    }
+                Divider()
+                    .opacity(isHidden ? 0 : 1)
+                HStack {
+                    ForEach(translateSources, id: \.self) { item in
+                        Button(action: {
+                            selectedTab = translateSources.firstIndex(of: item)!
+                        }) {
+                            Text(item)
+                                .foregroundColor(selectedTab == translateSources.firstIndex(of: item)! ? Color.blue : Color.gray)
+                                .padding()
+                        }
+                    }
+                }
+                .opacity(isHidden ? 0 : 1)
+                TabView(selection: $selectedTab) {
+                    // Baidu
+                    Text(translatedText1)
+                        .font(.system(size: 16))
+                        .foregroundColor(.black)
+                        .frame(maxWidth: .infinity, maxHeight: .infinity, alignment: .topLeading)
+                        .padding()
+                        .tag(0)
+                    // DeepL
+                    
+                    Text(translatedText2)
+                        .font(.system(size: 16))
+                        .foregroundColor(.black)
+                        .frame(maxWidth: .infinity, maxHeight: .infinity, alignment: .topLeading)
+                        .padding()
+                        .tag(1)
+                    // Azure
+                    
+                    Text(translatedText3)
+                        .font(.system(size: 16))
+                        .foregroundColor(.black)
+                        .frame(maxWidth: .infinity, maxHeight: .infinity, alignment: .topLeading)
+                        .padding()
+                        .tag(2)
+                    
+                }
+                .tabViewStyle(PageTabViewStyle(indexDisplayMode: .never))
+                .opacity(isHidden ? 0 : 1)
+                Spacer()
+                
+                Button {
+                    speechToText.toggleRecording()
+                    originalText = speechToText.transcript
+                    if speechToText.isRecording == false {
+                        speechToText.transcript = ""
+                        fetchTranslations()
+                        if isHidden == true {
+                            isHidden.toggle()
+                        }
+                        
+                    }
+                    
+                } label: {
+                    Image(systemName: speechToText.isRecording ? "mic.slash.fill" : "mic.fill")
+                        .resizable()
+                        .frame(width: 35, height: 50)
+                }
+                .frame(width: 100, height: 100)
+                .foregroundColor(.white)
+                .background(.blue)
+                .cornerRadius(50)
+                
+                HStack {
+                    // Language Switching
+                    Picker("Left Language", selection: $leftLanguage) {
+                        ForEach(leftLanguageOptions, id: \.self) { language in
+                            Text(language)
+                        }
+                    }
+                    .frame(width: 120, height: 35)
+                    .background(Color(UIColor.systemGray4))
+                    .cornerRadius(8)
+                    
+                    Button(action: {
+                        withAnimation {
+                            swap(&leftLanguage, &rightLanguage)
+                        }
+                    }) {
+                        Image(systemName: "arrow.left.arrow.right")
+                    }
+                    
+                    Picker("Right Language", selection: $rightLanguage) {
+                        ForEach(rightLanguageOptions, id: \.self) { language in
+                            Text(language)
+                        }
+                    }
+                    .frame(width: 120, height: 35)
+                    .background(Color(UIColor.systemGray4))
+                    .cornerRadius(8)
+                }
+                .onChange(of: leftLanguage) { _ in
+                    if leftLanguage == rightLanguage {
+                        rightLanguage = rightLanguageOptions.first!
+                    }
+                }
+                .onChange(of: rightLanguage) { _ in
+                    if leftLanguage == rightLanguage {
+                        leftLanguage = leftLanguageOptions.first!
+                    }
+                }
+                .padding()
+            }
+            // add rounded corner on top
+            .padding()
+            .background(Color(UIColor.systemGray6))
+            .cornerRadius(20)
+        }
+        .edgesIgnoringSafeArea(.bottom)
+        
     }
 }
 
+
 struct HomePageView_Previews: PreviewProvider {
     static var previews: some View {
-        HomePageView()
+        HomePageView(from: "auto", to: "zn")
     }
 }
---
+++ TranslateApp/Views/SpeechToText.swift
@@ -0,0 +1,78 @@
+//
+//  SpeechToText.swift
+//  TranslateApp
+//
+//  Created by Xiaolong Guo on 12/10/2023.
+//
+
+import AVFoundation
+import Speech
+import SwiftUI
+
+class SpeechToText: ObservableObject {
+    private var audioEngine = AVAudioEngine()
+    private var speechRecognizer = SFSpeechRecognizer(locale: Locale(identifier: "en_US"))
+    private var recognitionRequest: SFSpeechAudioBufferRecognitionRequest?
+    private var recognitionTask: SFSpeechRecognitionTask?
+
+    @Published var isRecording = false
+    @Published var transcript = ""
+    private var lastTranscript = ""
+
+    func toggleRecording() {
+        if isRecording {
+            audioEngine.stop()
+            recognitionRequest?.endAudio()
+            audioEngine.inputNode.removeTap(onBus: 0) // 移除 tap
+        } else {
+            do {
+                try startRecording()
+            } catch {
+                print("Could not start recording: \(error)")
+            }
+        }
+        isRecording.toggle()
+    }
+
+    private func startRecording() throws {
+        if let recognitionTask = recognitionTask {
+            recognitionTask.cancel()
+            self.recognitionTask = nil
+        }
+
+        let audioSession = AVAudioSession.sharedInstance()
+        try audioSession.setCategory(.record, mode: .measurement, options: .duckOthers)
+        try audioSession.setActive(true, options: .notifyOthersOnDeactivation)
+
+        recognitionRequest = SFSpeechAudioBufferRecognitionRequest()
+        if let recognizer = speechRecognizer, recognizer.supportsOnDeviceRecognition {
+    recognitionRequest?.requiresOnDeviceRecognition = true
+}
+
+        guard let recognitionRequest = recognitionRequest else { return }
+
+        let inputNode = audioEngine.inputNode
+        recognitionTask = speechRecognizer?.recognitionTask(with: recognitionRequest, resultHandler: { result, error in
+    if let result = result {
+        DispatchQueue.main.async {
+            let newTranscript = result.bestTranscription.formattedString
+            if self.lastTranscript != newTranscript {
+                let newContent = String(newTranscript.dropFirst(self.lastTranscript.count))
+                self.transcript += " " + newContent
+                self.lastTranscript = newTranscript
+            }
+        }
+    } else if let error = error {
+        print("Recognition error: \(error)")
+    }
+})
+
+        let recordingFormat = inputNode.outputFormat(forBus: 0)
+        inputNode.installTap(onBus: 0, bufferSize: 1024, format: recordingFormat) { buffer, _ in
+            recognitionRequest.append(buffer)
+        }
+
+        audioEngine.prepare()
+        try audioEngine.start()
+    }
+}
